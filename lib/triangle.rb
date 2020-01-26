baac48fe14aebfc4725aed95fa1b4bcff5063dd5
Ludmila Korchnoy <lkorchnoy@yahoo.com>
2020-01-26 07:54:35 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
 class Triangle
-  # write code here
+  attr_accessor :side_one, :side_two, :side_three
+  
+  def initialize(attributes)
+   attributes.each {|key, value| self.send(("#{key}="), value)} 
+end
+
+def triangle
+if side_one = side_two + side_three
+      begin
+        raise TriangleError
+      rescue TriangleError => error
+          puts error.message
+      end
+    else
+      it's a triangle
+    end 
+  end
+  
+  
+  
+
+
+
+
+class TriangleError < StandardError
+  def message
+    "The sum of the lengths of any two sides of a triangle always exceeds the length of the third side."
+  end
+
 end
